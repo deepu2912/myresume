f8a1c1a56f1264e635f0fa287fb2ab3e950054c6
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-06-05 04:05:32 -0700

    update name

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -8,7 +8,7 @@ export default class navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a className="navbar-brand js-scroll-trigger" href="#page-top"
-                ><span className="d-block d-lg-none">Deepanshu Ahuj</span><span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={logo} alt="" /></span></a
+                ><span className="d-block d-lg-none">Deepanshu Ahuja</span><span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={logo} alt="" /></span></a
             ><button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav">
